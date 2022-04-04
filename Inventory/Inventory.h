27bbbb9cd7762597783eb4da109609501adfc143
MiRu <miru121418@gmail.com>
2022-04-04 20:03:41 +0900

    step4 assignment
    이딴게... 최적화?

--- Inventory/Inventory.h
+++ Inventory/Inventory.h
@@ -1,44 +1,74 @@
 #pragma once
-#include<initializer_list>
 
-struct Item
-{
-	Item() : id(-999) { }
-	Item(std::initializer_list<int> li) {
-		id = *li.begin();
-	}
-	int id;
+#include <initializer_list>
+
+#include "InventoryException.h"
+
+enum ITEMID : long {ERROR = -1, NONE, Etc1, Etc2} ;
+
+struct Item{
+	Item(int index = ERROR) : _id(index) {}
+
+	int _id;
 };
 
-class Inventory
-{
+class Inventory{
 public:
-	Inventory(int itemNum);
-	Inventory(const Inventory& copy);
+	// ctor Group
+	Inventory() = default;
+	Inventory(int itemNum) : _ItemNum(itemNum){
+		_ItemList = new(std::nothrow) Item[itemNum];
+		for (int i = 0; i < _ItemNum; i++) _ItemList[i] = static_cast<Item>(NONE);
+	}
+	Inventory(const Inventory& copy) {
+		// 1
+		if (this == &copy) return;
+		if (_ItemList) delete[] _ItemList;
 
+		_ItemNum = copy._ItemNum;
+		_ItemList = new(std::nothrow) Item[_ItemNum];
+
+		for (int i = 0; i < _ItemNum; i++) _ItemList[i] = copy._ItemList[i];
+		// 2
+		// this.operator=(copy);
+	}
 	template<typename T>
-	Inventory(std::initializer_list<T> list) {
-		mItemNum = list.size();
-		mItems = new Item[list.size()];
+	Inventory(const std::initializer_list<T> itemindexlist) {
+		_ItemNum = itemindexlist.size();
+		_ItemList = new(std::nothrow) Item[_ItemNum];
 
 		int i = 0;
-		for (auto it : list)
-		{
-			mItems[i++] = Item{ it };
-		}
+		for (auto ItemIndex : itemindexlist) _ItemList[i++] = Item(ItemIndex);
 	}
-	~Inventory();
 
-public:
-	Inventory& operator=(const Inventory& rhs);
-	Item& operator[](const int index);
+	// dtor
+	~Inventory() {
+		delete[] _ItemList;
+	}
 
-public:
-	void AddItem(int index, int itemID);
-	void RemoveItem(int index);
-	void SwapItem(int target1, int target2);
+	// Operator Overroding Group
+	const Inventory& operator=(const Inventory& rhs) {
+		if (this == &rhs) return *this;
+		if (_ItemList) delete[] _ItemList;
+
+		_ItemNum = rhs._ItemNum;
+		_ItemList = new(std::nothrow) Item[_ItemNum];
+
+		for (int i = 0; i < _ItemNum; i++) _ItemList[i] = rhs._ItemList[i];
+
+		return *this;
+	}
+	Item& operator[](const int index) {
+		return _ItemList[index];
+	}
+
+	// Class Method Group
+	void AddItem(int, int);
+	void RemoveItem(int);
+	void SwapItem(int, int);
 
 private:
-	Item* mItems = nullptr;
-	int mItemNum;
+	int _ItemNum = 0;
+
+	Item* _ItemList = nullptr;
 };
